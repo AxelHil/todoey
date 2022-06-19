1935f9ee89d197612818fdecd2c7090bdfa83a1c
Axel Hil <axelhil@Axels-Mac-mini.fritz.box>
2022-06-19 22:34:49 +1200

    Add table and navigation controllers

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let listItemArray = ["ToDoItem1", "ToDoItem2", "ToDoItem3"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listItemArray.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoListItemCell")
+        var content = cell?.defaultContentConfiguration()
+        
+        content?.text = listItemArray[indexPath.row]
+        
+        cell?.contentConfiguration = content
+        
+        return cell!
+
+    }
+
+}
+
